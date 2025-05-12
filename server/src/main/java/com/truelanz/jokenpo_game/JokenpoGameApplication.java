009d0b4fe9710e9db94f72aa6027c37b655833c5
truelanz <devalanoliveira@gmail.com>
2025-05-12 16:45:12 -0300

    start project

---
+++ server/src/main/java/com/truelanz/jokenpo_game/JokenpoGameApplication.java
@@ -0,0 +1,13 @@
+package com.truelanz.jokenpo_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokenpoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokenpoGameApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/truelanz/jokenpo_game/JokenpoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truelanz.jokenpo_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokenpoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
